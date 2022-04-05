6cfdf1830d82b6054aadb35aaee56f7d1c2d9290
mc_fdc <45692264+tuna2134@users.noreply.github.com>
2022-04-05 21:56:30 +0900

    Update image.py

--- image.py
+++ image.py
@@ -1,6 +1,10 @@
 import requests
+import random, string
 
-with open("url.txt", "r") as f:
+def randomname(n):
+   return ''.join(random.choices(string.ascii_letters + string.digits, k=n))
+
+with open("urls_sexy.txt", "r") as f:
     lines = f.readlines()
     
 for url in lines:
@@ -10,5 +14,5 @@ for url in lines:
     except:
         continue
     else:
-        with open("", "wb") as f:
+        with open(randomname(10) + ".jpg", "wb") as f:
             f.write(r.content)
