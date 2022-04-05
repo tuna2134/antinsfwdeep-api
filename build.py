4bf13db65cd3dded9cfbaf4d58462b17b7e81931
mc_fdc <45692264+tuna2134@users.noreply.github.com>
2022-04-05 21:50:12 +0900

    Create build.py

---
+++ build.py
@@ -0,0 +1,18 @@
+from tensorflow import keras
+from tensorflow.keras import keras
+
+model = keras.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.Flatten())
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(10, activation='softmax'))
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(train_images, train_labels, epochs=5)
